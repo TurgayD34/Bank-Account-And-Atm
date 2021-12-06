69000292673edeb38fe7ab8d8221051881a47ab2
Turgay Demir <tdmr@Turgays-MacBook-Pro-2.local>
2021-12-06 15:17:41 +0100

    Updated

--- account.js
+++ account.js
@@ -9,33 +9,40 @@ function optionMenu(){
     5. Exit`);
     if(atm_functions == 1){
         alert(`Your avaialable balance is $${this.availableBal}`);
-    toContinue();
+        toContinue();
     }else if(atm_functions == 2){
-        var deposit= parseInt(prompt(`How much do you want to deposit?`));
+        var deposit = parseInt(prompt(`How much do you want to deposit?`));
         if (isNaN(deposit) || deposit === " ") {
             alert('Error: please enter a number!');
-           deposit();
+            var deposit = parseInt(prompt(`How much do you want to deposit?`));
         }
         availableBal += deposit;
-        alert(`You have successfully deposited ${deposit}, You now have ${this.availableBal}`); 
+        alert(`You have successfully deposited $${deposit}, You now have $${this.availableBal}`); 
         toContinue();
     }else if(atm_functions == 3){
         var withdrawal = parseInt(prompt(`How much do you want to withdrawal?`));
-        if (isNaN(withdrawal) || withdrawal === " ") {
+        if(withdrawal !== " " || withdrawal !== null || isNaN(withdrawal)){
+            if(availableBal < withdrawal){
+                alert(`You can not withdrawal $${withdrawal} amount.`)
+                var withdrawal = parseInt(prompt(`How much do you want to withdrawal?`));
+            }else{
+                availableBal -= withdrawal;
+                alert(`You have successfully deposited $${withdrawal}, You now have $${this.availableBal}`);
+            }
+        }else{
             alert('Error: please enter a number!');
-           deposit();
+            var withdrawal = parseInt(prompt(`How much do you want to withdrawal?`));
         }
-        availableBal -= withdrawal;
-        alert(`You have successfully deposited ${withdrawal}, You now have ${this.availableBal}`);
         toContinue();
     }else if(atm_functions == 4){
         var fullName = window.prompt(`Enter your name : `);
         alert(`Account name is ${fullName}`);
         toContinue();
     }else if(atm_functions == 5){
-        Exit();
+        return;
     }else{
         alert("Please make a valid selection");
+        toContinue();
     }
 }
 
@@ -46,7 +53,7 @@ function toContinue(){
     1.Yes
     2. No`));
     if(Yes_No === 2){
-       exit();
+       return;
     }
     else{
         return optionMenu(); 
