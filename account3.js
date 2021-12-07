35ec25b6633077e052315427d279102f4e37e30a
Turgay Demir <tdmr@Turgays-MacBook-Pro-2.local>
2021-12-07 05:58:56 +0100

    Acc 3rd ways

--- account3.js
+++ account3.js
@@ -1,6 +1,6 @@
 const getAccount = {
-    availableBal = 100,
-    accName = "Tdemir",
+    availableBal: 100,
+    accName: "Tdemir",
     getBalance: function(){
         alert(`Your avaialable balance is $${this.availableBal}`);
     },
@@ -10,17 +10,17 @@ const getAccount = {
             alert('Error: please enter a number!');
             var deposit = parseInt(prompt(`How much do you want to deposit?`));
         }
-        availableBal += deposit;
+        this.availableBal += deposit;
         alert(`You have successfully deposited $${deposit}, You now have $${this.availableBal}`); 
     },
     withdrawal: function(){
         var withdrawal = parseInt(prompt(`How much do you want to withdrawal?`));
         if(withdrawal !== " " || withdrawal !== null || isNaN(withdrawal)){
-            if(availableBal < withdrawal){
+            if(this.availableBal < withdrawal){
                 alert(`You can not withdrawal $${withdrawal} amount.`)
                 var withdrawal = parseInt(prompt(`How much do you want to withdrawal?`));
             }else{
-                availableBal -= withdrawal;
+                this.availableBal -= withdrawal;
                 alert(`You have successfully deposited $${withdrawal}, You now have $${this.availableBal}`);
             }
         }else{
@@ -50,7 +50,6 @@ const getAccount = {
     }
 }
 function optionMenu(){
-    const getAcc = getAccount();
     var atm_functions = window.prompt(`
     Hello,
     1. See Balance 
@@ -58,10 +57,23 @@ function optionMenu(){
     3. Maka a Withdrawal
     4. Get account name
     5. Exit`);
-    switch(getAcc){
-        case Balance:
-           this.getBalance();
-           this.toContinue(); 
+    if(atm_functions == 1){
+        getAccount.getBalance();
+        getAccount.toContinue();
+    }else if(atm_functions == 2){
+        getAccount.deposit();
+        getAccount.toContinue();
+    }else if(atm_functions == 3){
+        getAccount.withdrawal();
+        getAccount.toContinue();
+    }else if(atm_functions == 4){
+        getAccount.getAccName();
+        getAccount.toContinue();
+    }else if(atm_functions == 5){
+        return;
+    }else{
+        alert("Please make a valid selection");
+        getAccount.toContinue();
     }
 }
 optionMenu();
